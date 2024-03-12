bfed9dc967674afaa3f1485369edcd54744f8b3c
probabilitys <635444435@qq.com>
2024-03-12 15:11:22 -0500

    fixed decryption and complete testing

--- tests/AESIntegrationTest.java
+++ tests/AESIntegrationTest.java
@@ -9,7 +9,6 @@ public class AESIntegrationTest {
     @Test
     public void encryptionTest() {
         String plainText = "54776F204F6E65204E696E652054776F";
-        
         String cipherText = AESImplementation.encrypt(plainText, key);
         String expectedCipherText = "29c3505f571420f6402299b31a02d73a";
         assertEquals(expectedCipherText, cipherText);
@@ -18,8 +17,8 @@ public class AESIntegrationTest {
     @Test
     public void decryptionTest() {
         String plainText = "29c3505f571420f6402299b31a02d73a";
-        String cipherText = AESImplementation.encrypt(plainText, key);
-        String expectedCipherText = "54776F204F6E65204E696E652054776F";
+        String cipherText = AESImplementation.decrypt(plainText, key);
+        String expectedCipherText = "54776f204f6e65204e696e652054776f";
         assertEquals(expectedCipherText, cipherText);
     }
 }
